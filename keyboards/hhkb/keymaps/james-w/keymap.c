2702018f3d099a1c9ce65565cf023bd10c785ca8
James Westby <james@jameswestby.net>
2020-03-20 18:28:12 +0000

    Add my HHKB layout.
    This is roughly the standard HHKB layout and FN layer, however:
      - LGUI sends ESC on tap
      - SpaceFN activates a different FN layer that has my standard fn
        mappings
      - There's a TENKEY layer that provides a numpad

---
+++ keyboards/hhkb/keymaps/james-w/keymap.c
@@ -0,0 +1,107 @@
+/*  -*-  eval: (turn-on-orgtbl); -*-
+ * default HHKB Layout
+ */
+#include QMK_KEYBOARD_H
+
+#define BASE 0
+#define HHKB 1
+#define PROG 2
+#define TENKEY 3
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    /* BASE Level: Default Layer
+     |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
+     | Esc   | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | -     | =   | \     | ` |
+     |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
+     | Tab   | Q | W | E | R | T | Y | U | I | O | P | [     | ]   | Backs |   |
+     |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
+     | Cont  | A | S | D | F | G | H | J | K | L | ; | '     | Ent |       |   |
+     |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
+     | Shift | Z | X | C | V | B | N | M | , | . | / | Shift | Fn0 |       |   |
+     |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
+
+          |------+----------+----------------------------+------+------|
+          | LAlt | LGUI/Esc | ******* Space/Prog ******* | RGUI | RAlt |
+          |------+----------+----------------------------+------+------|
+    */
+
+    [BASE] = LAYOUT( //  default layer
+        KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLS, KC_GRV,
+        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC,
+        KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, MO(HHKB),
+        KC_LALT, LGUI_T(KC_ESC), LT(PROG, KC_SPC), KC_RGUI, KC_RALT),
+
+    /* Layer HHKB: HHKB mode (HHKB Fn)
+      |-------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
+      | Reset | F1  | F2  | F3  | F4 | F5 | F6 | F7 | F8  | F9  | F10 | F11 | F12   | Ins   | Del |
+      |-------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
+      | Caps  |     |     |     |    |    |    |    | Psc | Slk | Pus | Up  |       | Backs |     |
+      |-------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
+      |       | VoD | VoU | Mut |    |    | *  | /  | Hom | PgU | Lef | Rig | Enter |       |     |
+      |-------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
+      |       |     |     |     |    |    | +  | -  | End | PgD | Dow |     |       |       |     |
+      |-------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
+
+                 |------+------+----------------------+------+------+
+                 | **** | **** | ******************** | **** | **** |
+                 |------+------+----------------------+------+------+
+
+     */
+
+    [HHKB] = LAYOUT(
+        RESET,   KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_INS, KC_DEL,
+        KC_CAPS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_SLCK, KC_PAUS, KC_UP, KC_TRNS, KC_BSPC,
+        KC_TRNS, KC_VOLD, KC_VOLU, KC_MUTE, KC_TRNS, KC_TRNS, KC_PAST, KC_PSLS, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT, KC_PENT,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PPLS, KC_PMNS, KC_END, KC_PGDN, KC_DOWN, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+
+
+    /* Layer PROG: Programming mode
+      |-------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
+      |       | F1  | F2  | F3  | F4 | F5 | F6 | F7 | F8  | F9  | F10 | F11 | F12   |       |     |
+      |-------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
+      |       |     |     |     |    |    |Mute|VolD|VolU |Next |Play |     |       |       |     |
+      |-------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
+      |       | Home|PgDwn| PgUp| End|    |Left|Down| Up  |Right|     |     |       |       |     |
+      |-------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
+      |       |     |     |     |    |    |    |    |     |     |     |     | TENKEY|       |     |
+      |-------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
+
+                 |------+------+----------------------+------+------+
+                 | **** | **** | ******************** | **** | **** |
+                 |------+------+----------------------+------+------+
+
+     */
+    [PROG] = LAYOUT(
+        KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE, KC_VOLD, KC_VOLU, KC_MNXT, KC_MPLY, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TG(TENKEY),
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+
+    /* Layer TENKEY: Numpad mode
+      |-------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
+      |       |     |     |     |    |    |    |    |     |     |     |     |       |       |     |
+      |-------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
+      |       |     |     |     |    |    |NLCK|  7 |  8  |  9  |     |     |       |       |     |
+      |-------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
+      |       |     |     |     |    |    |    |  4 |  5  |  6  |     |     |       |       |     |
+      |-------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
+      |       |     |     |     |    |  0 |  1 |  2 |  3  |     |     |     | Toggle|       |     |
+      |-------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
+
+                 |------+------+----------------------+------+------+
+                 | **** | **** | ******************** | **** | **** |
+                 |------+------+----------------------+------+------+
+
+     */
+    [TENKEY] = LAYOUT(
+        KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NUMLOCK, KC_KP_7, KC_KP_8, KC_KP_9, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_KP_4, KC_KP_5, KC_KP_6, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_KP_0, KC_KP_1, KC_KP_2, KC_KP_3, KC_TRNS, KC_TRNS, KC_TRNS, TG(TENKEY),
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
+
+};
