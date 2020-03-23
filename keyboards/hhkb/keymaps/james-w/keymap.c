ccac2470d5ecff7b03c7fb01c394dbedc1c38f80
James Westby <james@jameswestby.net>
2020-03-23 11:33:14 +0000

    Make the layout a little closer to my nk65
    - Use GESC
    - Map ESC to ` in PROG
    - Map BKSP to DEL in PROG

--- keyboards/hhkb/keymaps/james-w/keymap.c
+++ keyboards/hhkb/keymaps/james-w/keymap.c
@@ -12,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* BASE Level: Default Layer
      |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
-     | Esc   | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | -     | =   | \     | ` |
+     | Esc/~ | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | -     | =   | \     | ` |
      |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
      | Tab   | Q | W | E | R | T | Y | U | I | O | P | [     | ]   | Backs |   |
      |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     */
 
     [BASE] = LAYOUT( //  default layer
-        KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLS, KC_GRV,
+        KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLS, KC_GRV,
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC,
         KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, MO(HHKB),
@@ -60,9 +60,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* Layer PROG: Programming mode
       |-------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
-      |       | F1  | F2  | F3  | F4 | F5 | F6 | F7 | F8  | F9  | F10 | F11 | F12   |       |     |
+      | `~    | F1  | F2  | F3  | F4 | F5 | F6 | F7 | F8  | F9  | F10 | F11 | F12   |       |     |
       |-------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
-      |       |     |     |     |    |    |Mute|VolD|VolU |Next |Play |     |       |       |     |
+      |       |     |     |     |    |    |Mute|VolD|VolU |Next |Play |     |       |       | Del |
       |-------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
       |       | Home|PgDwn| PgUp| End|    |Left|Down| Up  |Right|     |     |       |       |     |
       |-------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
@@ -75,8 +75,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
      */
     [PROG] = LAYOUT(
-        KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE, KC_VOLD, KC_VOLU, KC_MNXT, KC_MPLY, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE, KC_VOLD, KC_VOLU, KC_MNXT, KC_MPLY, KC_TRNS, KC_TRNS, KC_DEL,
         KC_TRNS, KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TG(TENKEY),
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
