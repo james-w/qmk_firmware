c7c6fd015da4499ee6a4e3d6de0faa3ef038e680
James Westby <james@jameswestby.net>
2020-05-11 18:00:25 +0100

    Add double tap shift for caps lock to polaris

--- keyboards/ai03/polaris/keymaps/james-w/keymap.c
+++ keyboards/ai03/polaris/keymaps/james-w/keymap.c
@@ -8,6 +8,20 @@
 #define PROG 2
 #define TENKEY 3
 
+//Tap Dance Declarations
+enum {
+  TD_LSFT_CAPS = 0,
+  TD_RSFT_CAPS = 1
+};
+
+//Tap Dance Definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+  //Tap once for Esc, twice for Caps Lock
+  [TD_LSFT_CAPS]  = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
+  [TD_RSFT_CAPS]  = ACTION_TAP_DANCE_DOUBLE(KC_RSFT, KC_CAPS)
+// Other declarations would go here, separated by commas, if you have them
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* BASE Level: Default Layer
@@ -30,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLS, KC_GRV,
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC,
         KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, MO(HHKB),
+        TD(TD_LSFT_CAPS), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, TD(TD_RSFT_CAPS), MO(HHKB),
         KC_NO, KC_LALT, LGUI_T(KC_ESC), LT(PROG, KC_SPC), KC_RGUI, KC_RALT, KC_NO),
 
     /* Layer HHKB: HHKB mode (HHKB Fn)
